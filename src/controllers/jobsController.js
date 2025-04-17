caf2d7b1df311efec6b880c4b735ed46c60db290
afnx <afndeveloper@gmail.com>
2025-04-17 15:37:21 -0700

    feat: set up db and create first query

--- src/controllers/jobsController.js
+++ src/controllers/jobsController.js
@@ -1,21 +1,8 @@
+
+const prisma = require('../db');
+
+
 exports.getJobs = async (req, res) => {
-    res.json({
-        message: 'Jobs fetched successfully',
-        data: [
-            {
-                id: 1,
-                title: 'Software Engineer',
-                company: 'Tech Company',
-                location: 'Remote',
-                description: 'Develop and maintain software applications.',
-            },
-            {
-                id: 2,
-                title: 'Data Scientist',
-                company: 'Data Corp',
-                location: 'New York, NY',
-                description: 'Analyze and interpret complex data sets.',
-            },
-        ],
-    });
+    const allJobs = await prisma.job.findMany();
+    res.json(allJobs);
 };
