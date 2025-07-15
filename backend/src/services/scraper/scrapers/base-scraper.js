5f97dfef4d749167bdb5dddb7257b2df250b1bf2
afnx <afndeveloper@gmail.com>
2025-07-15 14:47:07 -0700

    feat(scraper): implement BaseScraper and IndeedScraper classes with error handling

---
+++ backend/src/services/scraper/scrapers/base-scraper.js
@@ -0,0 +1,20 @@
+const { ERROR_TYPES, ScraperServiceError } = require('../../../utils/errors');
+
+class BaseScraper {
+    /**
+     * Abstract method to scrape data based on the provided query.
+     * Subclasses must implement this method to perform data scraping.
+     *
+     * @param {Object} query - The query parameters for scraping data.
+     * @throws {Error} Throws an error if not implemented by subclass.
+     * @returns {Promise<Array<Object>>} A promise that resolves to an array of data objects.
+     */
+    async scrape(query) {
+        throw new ScraperServiceError(
+            'scrape() must be implemented by subclass',
+            ERROR_TYPES.NOT_IMPLEMENTED,
+            501
+        );
+    }
+}
+module.exports = BaseScraper;
