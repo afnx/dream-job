ea43240fed8a0b6a347ca7c40ed3c332c3db3d9d
afnx <afndeveloper@gmail.com>
2025-09-04 16:53:28 -0700

    feat: add RibbonCreditBanner component to RootLayout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { RibbonCreditBanner } from "@/components/RibbonCreditBanner";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <RibbonCreditBanner />
         {children}
       </body>
     </html>
