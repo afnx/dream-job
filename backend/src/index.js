15ae4e9a2110e33f12644ded122894bfd68270f8
afnx <afndeveloper@gmail.com>
2025-09-18 22:13:55 -0700

    refactor: conditionally log server status and errors in development environment

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,5 +2,8 @@ const env = require('./config/env');
 const app = require('./app');
 
 app.listen(env.port, () => {
-    console.log(`Server is running on port ${env.port}`);
+    if (env.env === 'dev') {
+        // eslint-disable-next-line no-console
+        console.log(`Server is running on port ${env.port}`);
+    }
 });
